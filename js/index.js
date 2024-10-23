ebaceeabfa7cb1f41b1c05bb37939755d081ce3c
Abao Xing <66980306+GitBioinformatics@users.noreply.github.com>
2024-10-23 17:08:02 +0800

    init

---
+++ js/index.js
@@ -0,0 +1,32 @@
+/**
+* Template Name: Active
+* Template URL: https://bootstrapmade.com/active-bootstrap-website-template/
+* Updated: Aug 07 2024 with Bootstrap v5.3.3
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+*/
+
+(function() {
+  "use strict";
+
+
+  /**
+   * Init swiper sliders
+   */
+  function initSwiper() {
+    document.querySelectorAll(".init-swiper").forEach(function(swiperElement) {
+      let config = JSON.parse(
+        swiperElement.querySelector(".swiper-config").innerHTML.trim()
+      );
+
+      if (swiperElement.classList.contains("swiper-tab")) {
+        initSwiperWithCustomPagination(swiperElement, config);
+      } else {
+        new Swiper(swiperElement, config);
+      }
+    });
+  }
+
+  window.addEventListener("load", initSwiper);
+
+})();
